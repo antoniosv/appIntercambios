33fd4290fc1a69fb8256f2477f31b6e2038c7045
antoniosv <sephiran44@hotmail.com>
2014-04-04 09:38:42 +0000

    View de informacion

---
+++ app/views/tramites/requisitos.blade.php
@@ -0,0 +1,15 @@
+@extends("layout")
+@section("content")
+  <div class="container">  
+    <div class="sectionHeader">
+      <h2 class="sectionTitle">Requisitos</h2>      
+    </div>
+
+    <div class="row">
+      <div class="col-sm-8">
+	<div class="sectionInfo">
+	  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue purus sed ipsum scelerisque, ultricies vestibulum dui posuere. Praesent vehicula eros velit, ac aliquam enim.</p>
+	</div>
+      </div>
+    </div>
+@stop
